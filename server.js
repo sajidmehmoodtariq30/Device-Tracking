746f0f64ab0e046601ed015ea5fd256217c571e6
sajidmehmoodtariq30 <sajidmehmoodtariq5@gmail.com>
2025-07-18 13:15:11 +0500

    Basic Setup

---
+++ server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+const server = createServer(app);
+const io = new Server(server);
+
+// Set static files directory
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Set view engine and views directory
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+server.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
